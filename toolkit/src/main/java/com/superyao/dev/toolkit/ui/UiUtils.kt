230f15c58bbbeb68d1e847be8f1062a24759a5cd
SUPERYAO <supersy943@gmail.com>
2021-09-05 22:30:02 +0800

    * add clickTooFast() to UiUtils

--- toolkit/src/main/java/com/superyao/dev/toolkit/ui/UiUtils.kt
+++ toolkit/src/main/java/com/superyao/dev/toolkit/ui/UiUtils.kt
@@ -8,6 +8,26 @@ import android.widget.EditText
 import androidx.core.widget.addTextChangedListener
 
 /*
+Prevent clicking too fast
+ */
+
+private var lastClickTime = 0L
+
+/**
+ * fun yourFunction() {
+ *  if (clickTooFast()) return
+ *  // Do something what you want
+ * }
+ */
+fun clickTooFast(interval: Int = 500): Boolean {
+    var result = false
+    val current = System.currentTimeMillis()
+    if (current - lastClickTime < interval) result = true
+    lastClickTime = current
+    return result
+}
+
+/*
 EditText
  */
 
